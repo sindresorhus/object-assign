478efc1a44e78574624afaa669e0d74a4d59f1e7
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-04 22:01:08 +0100

    move it closer to the spec

--- object-assign.js
+++ object-assign.js
@@ -8,15 +8,36 @@
 (function () {
 	'use strict';
 
+	var ToObject = function (val) {
+		if (val == null) {
+			throw new TypeError();
+		}
+
+		return Object(val);
+	}
+
 	var objectAssign = Object.assign || function (target, source) {
-		var keys = Object.keys(source);
+		var pendingException;
+		var to = ToObject(target);
+		var from = ToObject(source);
+		var keys = Object.keys(from);
 		var i = keys.length;
 
 		while (i--) {
-			target[keys[i]] = source[keys[i]];
+			try {
+				to[keys[i]] = from[keys[i]];
+			} catch (err) {
+				if (pendingException === undefined) {
+					pendingException = err;
+				}
+			}
 		}
 
-		return target;
+		if (pendingException) {
+			throw pendingException;
+		}
+
+		return to;
 	};
 
 	if (typeof module !== 'undefined' && module.exports) {
--- test.js
+++ test.js
@@ -5,3 +5,12 @@ var assign = require('./object-assign');
 it('should assign own enumerable properties from source to target object', function () {
 	assert.deepEqual(assign({foo: 0}, {bar: 1}), {foo: 0, bar: 1});
 });
+
+it('should throw on `null` and `undefined`', function () {
+	assert.throws(function () {
+		assign(null, {});
+		assign({}, null);
+		assign(undefined, {});
+		assign({}, undefined);
+	});
+});
