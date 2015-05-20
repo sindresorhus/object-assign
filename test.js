dad189072d882c64db8bc89527ee2d44c446f89b
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-20 22:03:44 +0200

    improve tests

--- test.js
+++ test.js
@@ -1,53 +1,54 @@
 'use strict';
 var assert = require('assert');
-var assign = require('./');
+Object.objectAssign = undefined;
+var objectAssign = require('./');
 
 it('should have the correct length', function () {
-	assert.equal(assign.length, 2);
+	assert.equal(objectAssign.length, 2);
 });
 
 it('should throw when target is not an object', function () {
-	assert.throws(function () { assign(null); }, TypeError);
-	assert.throws(function () { assign(undefined); }, TypeError);
+	assert.throws(function () { objectAssign(null); }, TypeError);
+	assert.throws(function () { objectAssign(undefined); }, TypeError);
 });
 
-it('should assign own enumerable properties from source to target object', function () {
-	assert.deepEqual(assign({foo: 0}, {bar: 1}), {foo: 0, bar: 1});
-	assert.deepEqual(assign({foo: 0}, null, undefined), {foo: 0});
-	assert.deepEqual(assign({foo: 0}, null, undefined, {bar: 1}, null), {foo: 0, bar: 1});
+it('should objectAssign own enumerable properties from source to target object', function () {
+	assert.deepEqual(objectAssign({foo: 0}, {bar: 1}), {foo: 0, bar: 1});
+	assert.deepEqual(objectAssign({foo: 0}, null, undefined), {foo: 0});
+	assert.deepEqual(objectAssign({foo: 0}, null, undefined, {bar: 1}, null), {foo: 0, bar: 1});
 });
 
 it('should throw on null/undefined target', function () {
 	assert.throws(function () {
-		assign(null, {});
+		objectAssign(null, {});
 	});
 
 	assert.throws(function () {
-		assign(undefined, {});
+		objectAssign(undefined, {});
 	});
 
 	assert.throws(function () {
-		assign(undefined, undefined);
+		objectAssign(undefined, undefined);
 	});
 });
 
 it('should not throw on null/undefined sources', function () {
 	assert.doesNotThrow(function () {
-		assign({}, null);
+		objectAssign({}, null);
 	});
 
 	assert.doesNotThrow(function () {
-		assign({}, undefined);
+		objectAssign({}, undefined);
 	});
 
 	assert.doesNotThrow(function () {
-		assign({}, undefined, null);
+		objectAssign({}, undefined, null);
 	});
 });
 
 it('should support multiple sources', function () {
-	assert.deepEqual(assign({foo: 0}, {bar: 1}, {bar: 2}), {foo: 0, bar: 2});
-	assert.deepEqual(assign({}, {}, {foo: 1}), {foo: 1});
+	assert.deepEqual(objectAssign({foo: 0}, {bar: 1}, {bar: 2}), {foo: 0, bar: 2});
+	assert.deepEqual(objectAssign({}, {}, {foo: 1}), {foo: 1});
 });
 
 it('should only iterate own keys', function () {
@@ -56,11 +57,11 @@ it('should only iterate own keys', function () {
 	var unicorn = new Unicorn();
 	unicorn.bar = 1;
 
-	assert.deepEqual(assign({foo: 1}, unicorn), {foo: 1, bar: 1});
+	assert.deepEqual(objectAssign({foo: 1}, unicorn), {foo: 1, bar: 1});
 });
 
 it('should return the modified target object', function () {
 	var target = {};
-	var returned = assign(target, { a: 1 });
+	var returned = objectAssign(target, { a: 1 });
 	assert.equal(returned, target);
 });
