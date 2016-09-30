ea6846a616f9a7678b59310c55a3147bf6bd157c
Sindre Sorhus <sindresorhus@gmail.com>
2016-09-30 12:09:38 +0700

    meta tweaks

--- readme.md
+++ readme.md
@@ -1,8 +1,6 @@
 # object-assign [![Build Status](https://travis-ci.org/sindresorhus/object-assign.svg?branch=master)](https://travis-ci.org/sindresorhus/object-assign)
 
-> ES2015 [`Object.assign()`](http://www.2ality.com/2014/01/object-assign.html) ponyfill
-
-> Ponyfill: A polyfill that doesn't overwrite the native method
+> ES2015 [`Object.assign()`](http://www.2ality.com/2014/01/object-assign.html) [ponyfill](https://ponyfill.com)
 
 
 ## Install
@@ -36,7 +34,7 @@ objectAssign({foo: 0}, null, {bar: 1}, undefined);
 
 ## API
 
-### objectAssign(target, source, [source, ...])
+### objectAssign(target, [source, ...])
 
 Assigns enumerable own properties of `source` objects to the `target` object and returns the `target` object. Additional `source` objects will overwrite previous ones.
 
--- test.js
+++ test.js
@@ -1,131 +1,129 @@
-'use strict';
-/* eslint-env mocha */
-var assert = require('assert');
+import test from 'ava';
 
-Object.assign = undefined;
-var objectAssign = require('./');
+Object.assign = require('./');
+const objectAssign = require('./');
 
-it('should have the correct length', function () {
-	assert.equal(objectAssign.length, 2);
+test('have the correct length', t => {
+	t.is(objectAssign.length, 2);
 });
 
-it('should throw when target is not an object', function () {
-	assert.throws(function () {
+test('throw when target is not an object', t => {
+	t.throws(() => {
 		objectAssign(null);
 	}, TypeError);
-	assert.throws(function () {
+	t.throws(() => {
 		objectAssign(undefined);
 	}, TypeError);
 });
 
-it('should objectAssign own enumerable properties from source to target object', function () {
-	assert.deepEqual(objectAssign({foo: 0}, {bar: 1}), {
+test('objectAssign own enumerable properties from source to target object', t => {
+	t.deepEqual(objectAssign({foo: 0}, {bar: 1}), {
 		foo: 0,
 		bar: 1
 	});
-	assert.deepEqual(objectAssign({foo: 0}, null, undefined), {foo: 0});
-	assert.deepEqual(objectAssign({foo: 0}, null, undefined, {bar: 1}, null), {
+	t.deepEqual(objectAssign({foo: 0}, null, undefined), {foo: 0});
+	t.deepEqual(objectAssign({foo: 0}, null, undefined, {bar: 1}, null), {
 		foo: 0,
 		bar: 1
 	});
 });
 
-it('should throw on null/undefined target', function () {
-	assert.throws(function () {
+test('throw on null/undefined target', t => {
+	t.throws(() => {
 		objectAssign(null, {});
 	});
 
-	assert.throws(function () {
+	t.throws(() => {
 		objectAssign(undefined, {});
 	});
 
-	assert.throws(function () {
+	t.throws(() => {
 		objectAssign(undefined, undefined);
 	});
 });
 
-it('should not throw on null/undefined sources', function () {
-	assert.doesNotThrow(function () {
+test('not throw on null/undefined sources', t => {
+	t.notThrows(() => {
 		objectAssign({}, null);
 	});
 
-	assert.doesNotThrow(function () {
+	t.notThrows(() => {
 		objectAssign({}, undefined);
 	});
 
-	assert.doesNotThrow(function () {
+	t.notThrows(() => {
 		objectAssign({}, undefined, null);
 	});
 });
 
-it('should support multiple sources', function () {
-	assert.deepEqual(objectAssign({foo: 0}, {bar: 1}, {bar: 2}), {
+test('support multiple sources', t => {
+	t.deepEqual(objectAssign({foo: 0}, {bar: 1}, {bar: 2}), {
 		foo: 0,
 		bar: 2
 	});
-	assert.deepEqual(objectAssign({}, {}, {foo: 1}), {foo: 1});
+	t.deepEqual(objectAssign({}, {}, {foo: 1}), {foo: 1});
 });
 
-it('should only iterate own keys', function () {
-	var Unicorn = function () {};
+test('only iterate own keys', t => {
+	const Unicorn = function () {};
 	Unicorn.prototype.rainbows = 'many';
-	var unicorn = new Unicorn();
+	const unicorn = new Unicorn();
 	unicorn.bar = 1;
 
-	assert.deepEqual(objectAssign({foo: 1}, unicorn), {
+	t.deepEqual(objectAssign({foo: 1}, unicorn), {
 		foo: 1,
 		bar: 1
 	});
 });
 
-it('should return the modified target object', function () {
-	var target = {};
-	var returned = objectAssign(target, {a: 1});
-	assert.equal(returned, target);
+test('return the modified target object', t => {
+	const target = {};
+	const returned = objectAssign(target, {a: 1});
+	t.is(returned, target);
 });
 
-it('should support `Object.create(null)` objects', function () {
-	var obj = Object.create(null);
+test('support `Object.create(null)` objects', t => {
+	const obj = Object.create(null);
 	obj.foo = true;
-	assert.deepEqual(objectAssign({}, obj), {foo: true});
+	t.deepEqual(objectAssign({}, obj), {foo: true});
 });
 
-it('should preserve property order', function () {
-	var letters = 'abcdefghijklmnopqrst';
-	var source = {};
-	letters.split('').forEach(function (letter) {
+test('preserve property order', t => {
+	const letters = 'abcdefghijklmnopqrst';
+	const source = {};
+	letters.split('').forEach(letter => {
 		source[letter] = letter;
 	});
-	var target = objectAssign({}, source);
-	assert.equal(Object.keys(target).join(''), letters);
+	const target = objectAssign({}, source);
+	t.is(Object.keys(target).join(''), letters);
 });
 
-it('should accept primitives as target', function () {
-	var target = objectAssign('abcdefg', {foo: 'bar'});
-	var strObj = Object('abcdefg');
+test('accept primitives as target', t => {
+	const target = objectAssign('abcdefg', {foo: 'bar'});
+	const strObj = Object('abcdefg');
 	strObj.foo = 'bar';
-	assert.deepEqual(target, strObj);
+	t.deepEqual(target, strObj);
 });
 
-if (typeof Symbol !== 'undefined') {
-	it('should support symbol properties', function () {
-		var target = {};
-		var source = {};
-		var sym = Symbol('foo');
+if (typeof global.Symbol !== 'undefined') {
+	test('support symbol properties', t => {
+		const target = {};
+		const source = {};
+		const sym = Symbol('foo');
 		source[sym] = 'bar';
 		objectAssign(target, source);
-		assert.equal(target[sym], 'bar');
+		t.is(target[sym], 'bar');
 	});
 
-	it('should only copy enumerable symbols', function () {
-		var target = {};
-		var source = {};
-		var sym = Symbol('foo');
+	test('only copy enumerable symbols', t => {
+		const target = {};
+		const source = {};
+		const sym = Symbol('foo');
 		Object.defineProperty(source, sym, {
 			enumerable: false,
 			value: 'bar'
 		});
 		objectAssign(target, source);
-		assert.equal(target[sym], undefined);
+		t.is(target[sym], undefined);
 	});
 }
